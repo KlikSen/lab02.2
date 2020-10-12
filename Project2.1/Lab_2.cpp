fcdfcd4546663b568bd2721d20f1d592860f0032
KlikSen <52624005+KlikSen@users.noreply.github.com>
2020-10-12 11:19:25 +0300

    Revert "Revert "Revert "Merge pull request #1 from KlikSen/branch_02"""
    This reverts commit 04681407fe9c949fda6340153ce78aae8e8df8f0.

--- Project2.1/Lab_2.cpp
+++ Project2.1/Lab_2.cpp
@@ -11,18 +11,18 @@ int main()
 {
 	double a;
 	double b;
-	//double z1;
-	double z2;
+	double z1;
+	//double z2;
 
 	cout << "a ="; cin >> a;
 	cout << "b ="; cin >> b;
 
-	//z1 = (sin(a) + cos(2 * b - a)) / (cos(a) - sin(2 * b - a));
-	z2 = (1 + sin(2 * b)) / cos(2 * b);
+	z1 = (sin(a) + cos(2 * b - a)) / (cos(a) - sin(2 * b - a));
+	//z2 = (1 + sin(2 * b)) / cos(2 * b);
 
 	cout << endl;
-	//cout << "z1 = " << z1 << endl;
-	cout << "z2 = " << z2 << endl;
+	cout << "z1 = " << z1 << endl;
+	//cout << "z2 = " << z2 << endl;
 
 	return 0;
 }
